10a4152667fe177055b1f02e9d9dbdbc9cf241c6
Joann Pan <joannlpan@gmail.com>
2021-10-22 15:16:33 -0400

    add login form component

---
+++ src/actions/currentUser.js
@@ -0,0 +1,24 @@
+//synchronous action creators
+export const setCurrentUser = user => {
+    return {
+        type: "SET_CURRENT_USER", 
+        payload: user
+    }
+}
+//returns an object
+
+//asynchronous action creators
+export const login = credentials => {
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({username: "rileyflynn", password: "password"})
+    }
+
+    return dispatch => {
+        return fetch("http://localhost:3001/api/v1/login", configObj)
+    }
+}
+//we are returning a function that accepts dispatch as an argument
