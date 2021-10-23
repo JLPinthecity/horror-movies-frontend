353ddc4eeb63edece915112a9772b3b62f5d7a1b
Joann Pan <joannlpan@gmail.com>
2021-10-23 11:13:48 -0400

    send both username and password to action creator

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,16 +2,14 @@ import React from "react";
 import { connect } from 'react-redux';
 import { updateLoginForm } from "../actions/loginForm";
 
-
 const Login = (props) => {
     
     const handleChange = event => {
         
         const { name, value } = event.target
         const updatedFormData = {
-            
-            name, 
-            value
+            ...props.loginForm
+            [name]: value
         }
         
         props.updateLoginForm(updatedFormData)
@@ -19,8 +17,8 @@ const Login = (props) => {
 
     return (
         <form >
-            <input placeholder="username" type="text" name="username" value={props.username} onChange={handleChange} />
-            <input placeholder="password" type="text" name="password" value={props.password} onChange={handleChange} />
+            <input placeholder="username" type="text" name="username" value={props.loginForm.username} onChange={handleChange} />
+            <input placeholder="password" type="text" name="password" value={props.loginForm.password} onChange={handleChange} />
             <input type="submit" />
         </form>
 
@@ -29,8 +27,6 @@ const Login = (props) => {
 
 const mapStateToProps = state => {
     return {
-        username: state.loginForm.username,
-        password: state.loginForm.password
-    }
+        loginForm: state.loginForm
 }
 export default connect(mapStateToProps, { updateLoginForm })(Login);
