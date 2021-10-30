b84d1fd02bc1024268b09ef414daac941a4986a9
Joann Pan <joannlpan@gmail.com>
2021-10-30 12:32:41 -0400

    import signupForm into store

--- src/store.js
+++ src/store.js
@@ -3,13 +3,15 @@ import usersReducer from './reducers/users.js'
 import currentUser from './reducers/currentUser.js';
 import loginForm from './reducers/loginForm.js';
 import horrorMovies from './reducers/horrorMovies.js';
+import signupForm from './reducers/signupForm.js';
 import thunk from 'redux-thunk';
 
 const reducer = combineReducers({
     usersReducer,
     currentUser,
     loginForm,
-    horrorMovies
+    horrorMovies,
+    signupForm
 })
   
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
