95ed4dc9d78fa2e5e00159f5d155d4bd3aede4bf
Joann Pan <joannlpan@gmail.com>
2021-10-30 14:35:08 -0400

    add handle submit and start new action creator for signup

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -5,17 +5,22 @@ import { updateSignupForm } from '../actions/signupForm';
 const SignUp = (props) => {
 
     const handleChange = (event) => {
+        
         const { name, value } = event.target
         const updatedFormData = {
             ...props.signupForm,
             [name]: value
         }
-
         props.updateSignupForm(updatedFormData)
     }
 
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        
+    }
+
     return (
-        <form onSubmit={console.log("test")}>
+        <form onSubmit={handleSubmit}>
             <input placeholder="name" type="text" name="name" value={props.signupForm.name} onChange={handleChange}/>
             <input placeholder="username" type="text" name="username" value={props.signupForm.username} onChange={handleChange}/>
             <input placeholder="password" type="text" name="password" value={props.signupForm.password} onChange={handleChange}/>
--- src/reducers/signupForm.js
+++ src/reducers/signupForm.js
@@ -6,10 +6,12 @@ const initialState = {
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state = initialState, action) => {
+    
     switch (action.type) {
 
         case "UPDATE_SIGNUP_FORM":
             return action.payload
+            debugger
 
         default:
             return state
