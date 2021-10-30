f85e8559763e4c393bc73317cc5932ce1a49db26
Joann Pan <joannlpan@gmail.com>
2021-10-30 12:30:40 -0400

    add initial state for signup form fields

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -4,6 +4,7 @@ const SignUp = () => {
 
     return (
         <form onSubmit={console.log("test")}>
+            <input placeholder="name" type="text" name="username"  />
             <input placeholder="username" type="text" name="username"  />
             <input placeholder="password" type="text" name="password"  />
             <input type="submit" />
---
+++ src/reducers/signupForm.js
@@ -0,0 +1,19 @@
+const initialState = {
+    name: "",
+    username: "",
+    password: ""
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state = initialState, action) => {
+    switch (action.type) {
+
+        case "UPDATE_SIGNUP_FORM":
+            return action.payload
+
+        default:
+            return state
+
+    }     
+
+}
