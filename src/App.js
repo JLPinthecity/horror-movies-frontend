1ecbe58456729dd8893c211f49717dc26ed963cf
Joann Pan <joannlpan@gmail.com>
2021-11-11 11:03:54 -0500

    import movie card into app component for route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LoginForm from './components/LoginForm';
 import Home from './components/Home';
 import ContentContainer from './components/ContentContainer';
 import SignUp from './components/SignUp';
+import MovieCard from './components/MovieCard'
 import NewMovieForm from './components/NewMovieForm';
 import { connect } from 'react-redux';
 import { getCurrentUser } from './actions/currentUser'
@@ -33,6 +34,8 @@ class App extends React.Component {
             <Route exact path="/horror-movies" component={ContentContainer}/>
 
             <Route exact path="/horror-movies/new" component={NewMovieForm}/>
+
+            <Route exact path="/horror-movies/:slug" component={MovieCard}
 
           </Switch>
       </div>
