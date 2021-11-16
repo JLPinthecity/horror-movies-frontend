c1ffb3c3e3502f8e69a973b2b0408a28b3917ae0
Joann Pan <joannlpan@gmail.com>
2021-11-16 13:59:42 -0500

    mapStateToProps to grab modal button status out of store

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const Home = () => {
+const Home = (props) => {
+    debugger
     return (
         <div>
-            <button className="openModalButton">LOGIN BUTTON TB</button>
+            <button className="openModalButton">Log In</button>
+
+            { props.button_clicked && <Modal/>}
+
         </div>
+
+        
     )
 }
 
-export default Home;
+const mapStateToProps = state => {
+    return {
+        button_clicked: state.openModalButton
+    }
+}
+
+//openModalButton's initial state is set to false 
+
+export default connect(mapStateToProps)(Home);
 
 
 /* Home is the homepage for all users NOT LOGGED IN, so will need to display all movies in DB by date with average user ratings for each  */
