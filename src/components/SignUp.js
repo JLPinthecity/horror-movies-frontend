390ee3463ead92367b190839c39a2c2cde413bc3
Joann Pan <joannlpan@gmail.com>
2021-11-17 13:33:50 -0500

    style signup modal

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -25,12 +25,45 @@ const SignUp = (props) => {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input placeholder="name" type="text" name="name" value={props.signupForm.name} onChange={handleChange}/>
-            <input placeholder="username" type="text" name="username" value={props.signupForm.username} onChange={handleChange}/>
-            <input placeholder="password" type="text" name="password" value={props.signupForm.password} onChange={handleChange}/>
-            <input type="submit"/>
-        </form>
+        <div className="modalBackground">
+            <div className="modalContainer">
+
+            <div className="first_row">
+                <div className="title">
+                Create an account
+                </div><br/><br/>
+
+                <div className="loginModalCloseButton">
+                    <button onClick={ props.openOrCloseModal }><Close/></button>
+                </div>
+            </div>
+
+            <div className="body">
+                <form onSubmit={handleSubmit}>
+                    <label>Name</label>
+                    <input type="text" name="name" value={props.signupForm.name} onChange={handleChange}/>
+                    <label>Username</label>
+                    <input type="text" name="username" value={props.signupForm.username} onChange={handleChange}/>
+                    <label>Password</label>
+                    <input type="text" name="password" value={props.signupForm.password} onChange={handleChange}/>
+                    <input className="submit-button" type="submit" value="Sign Up" />
+                </form>
+            </div>
+
+            <div className="footer">
+                Already have an account? 
+                <br/><br/>
+                <Link className="login_btn" to="/login">Sign in here.</Link>
+                <br/><br/>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+        
 
     )
 
