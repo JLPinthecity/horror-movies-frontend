8fd3f665b3f95465a8d72963a5067b4aac4d0096
Joann Pan <joannlpan@gmail.com>
2021-11-19 18:09:24 -0500

    add divs to movie attributes

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,13 +3,25 @@ import React from 'react';
 const MovieCard = ({ movie }) => {
     return (
         <div className="card">
-            <div className="card">
-                <img src={movie.attributes.poster} alt="horror movie poster" width="500" />
+            <div className="poster">
+                <img src={movie.attributes.poster} alt={movie.attributes.title} width="500" />
             </div>
             
-            <h3>{movie.attributes.title}</h3>
-            <p>{movie.attributes.rating}</p>
-            <p>{movie.attributes.director}</p>
+            <div className="title">
+                <h3>{movie.attributes.title}</h3>
+            </div>
+
+            <div className="rating">
+                <p>{movie.attributes.avg_score}</p>
+            </div>
+
+            <div className="title">
+                <p>{movie.attributes.director}</p>
+            </div>
+
+            <div className="link">
+                <a href={`/horror_movies/${props.attributes.slug}`}>View Movie</a>
+            </div>
             
         </div>
     )
