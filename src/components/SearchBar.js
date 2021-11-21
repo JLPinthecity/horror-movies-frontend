ac18642ee16cb96d75eed3a0df036f531a9d9e0b
Joann Pan <joannlpan@gmail.com>
2021-11-21 13:58:14 -0500

    close button on search component works

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { Close } from '@material-ui/icons';
 import "./Search.css";
+import { connect } from 'react-redux';
+import { showOrHideSearchBar } from '../actions/showOrHideSearchBar';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+
     return (
         <div className="wrap">
     
             <div className="search search-open">
 
                 <form className="searchform">
-                <input type="search" name="search" value="" placeholder="Search our movies" className="input_box"/>
+                    <input type="search" name="search" value="" placeholder="Search our movies" className="search-input"/>
                 </form>
 
-                <span className="remove-search"><Close/></span>
+                <span className="remove-search" onClick={ props.showOrHideSearchBar }><Close/></span>
             </div>
 
         </div>
     )
 }
 
-export default SearchBar;
+export default connect(null, { showOrHideSearchBar })(SearchBar);
