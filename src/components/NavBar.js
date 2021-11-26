d1e7699121acad5b571b90676e15e9895af4f2f1
Joann Pan <joannlpan@gmail.com>
2021-11-26 14:12:11 -0500

    saving progress on menu sidebar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -60,16 +60,14 @@ const NavBar = (props) => {
 
             <div className="menu-container">
 
-                <div className="menu-row">
-                    <span className={props.menuButtonClicked ? "menu-title" : "hide"}>Menu</span>
-
-                    <span className={props.menuButtonClicked ? "menu-close" : "hide"}><Close/></span>
-
-                </div>
-
-            
-
                 <ul className="menu" id={props.menuButtonClicked ? "hidden" : ""}>
+
+                    <div className="menu-row">
+                        <span className={props.menuButtonClicked ? "menu-title" : "hide"}>Menu</span>
+
+                        <span className={props.menuButtonClicked ? "menu-close" : "hide"}><Close onClick={handleChange}/></span>
+
+                    </div>
 
                     <li><Link to="/">HOME</Link></li>
                     <li><Link to="/about">ABOUT</Link></li>
