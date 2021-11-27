356221b2df08ed06e1131b9a9bf6941d627c14fa
Joann Pan <joannlpan@gmail.com>
2021-11-27 14:57:18 -0500

    edit home component to connect to store

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux'
 
 const Home = (props) => {
 
-   
-    return (
-        <div>
-            Home
-        </div>
 
-        
+    return (
+
+        <div>
+            <img
+            src={this.state.img}
+            onMouseEnter={() => {
+                this.setState({
+                img: "http://www.toptipsclub.com/Images/page-img/keep-calm-and-prepare-for-a-test.png"
+                })
+            }}
+
+            onMouseOut={() => {
+                this.setState({
+                img: "https://i.vimeocdn.com/portrait/58832_300x300"
+                })
+            }}
+            />
+        </div>  
     )
 }
 
-
+const mapStateToProps = state => {
+    switchImage: state.backgroundImage
+}
 
 //openModalButton's initial state is set to false 
 
-export default Home;
+export default connect(Home);
 
 /* Home is the homepage for all users NOT LOGGED IN, so will need to display all movies in DB by date with average user ratings for each  */
