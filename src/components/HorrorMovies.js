a84c547a99b018122a5e14718f48efd802ed8d8b
Joann Pan <joannlpan@gmail.com>
2021-11-29 18:03:24 -0500

    create stylesheet for horror movies component

--- src/components/HorrorMovies.js
+++ src/components/HorrorMovies.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import MovieCard from './MovieCard';
+import "./HorrorMovies.css";
 
 const HorrorMovies = (props) => {
     const movieCards = props.movies.length > 0 ? props.movies.map(m => <MovieCard movie={m} key={m.attributes.id}/>) : null
