9a6aa28cbe65d803ccc193ba9ceb902277351e77
Joann Pan <joannlpan@gmail.com>
2021-12-01 11:17:00 -0500

    create header section for movies for horror movie component

---
+++ src/components/MovieHeader.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header-wrapper">
+            <h1>Movie Name</h1>
+
+            <div>
+            <div className="total-reviews"> </div>
+            <div className="star-rating"> </div>
+            <div className="total-out-of"> </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Header;
