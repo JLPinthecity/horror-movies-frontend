54832d28e7978258640678b1a5d9d9baa6cdbe3a
Joann Pan <joannlpan@gmail.com>
2021-12-02 13:39:14 -0500

    add outline to individual movie show component

--- src/components/IndividualMovieShow.js
+++ src/components/IndividualMovieShow.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { getMoviePlusReviews } from '../actions/individualMovie'
 import { connect } from 'react-redux';
 import MovieHeader from './MovieHeader';
+import "./IndividualMovieShow.css";
 
 class IndividualMovieShow extends React.Component {
 
     componentDidMount() {
         const url = this.props.match.url
         this.props.getMoviePlusReviews(url)
-        debugger
     }
 
     render() {
@@ -16,12 +16,25 @@ class IndividualMovieShow extends React.Component {
 
         return (
             <div className="movie-wrapper">
+
                 <div className="column-1">
-                    { this.props.loaded && <MovieHeader/>}
+
+                    <div className ="main">
+
+                        { this.props.loaded && <MovieHeader/>}
+
+                    </div>
+
+                    <div className="reviews">
+
+                    </div>
 
                 </div>
 
                 <div className="column-2">
+                    <div className="review-form">
+                        FORM GOES HERE
+                    </div>
             
                 </div>
 
--- src/components/MovieHeader.js
+++ src/components/MovieHeader.js
@@ -2,20 +2,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+
 const MovieHeader = (props) => {
 
     const { poster, title, director, year_released, slug, average_score } = props.attributes
     const { reviews } = props
-    
+
     return (
         <div className="header-wrapper">
            
             <h1><img src={ poster } alt={ title }/> { title }</h1>
 
-            <div>
-            <div className="total-reviews">{ reviews.length } user reviews</div>
-            <div className="star-rating"> </div>
-            <div className="total-out-of">{ average_score } out of 5</div>
+            <div className="review_details">
+                <div className="total-reviews">{ reviews.length } user reviews</div>
+                <div className="star-rating"> </div>
+                <div className="total-out-of">{ average_score } out of 5</div>
             </div>
 
         </div>
